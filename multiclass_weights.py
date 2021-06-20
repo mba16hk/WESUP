f3ec64d9e9cd2513f8a553fca575f07bc5a69094
Hiba Khalidi <hk502@cam.ac.uk>
2021-06-20 11:55:59 +0100

    Updated script to calculate weights for multi-class annotations

--- multiclass_weights.py
+++ multiclass_weights.py
@@ -9,7 +9,6 @@ import os
 import glob
 import argparse
 import itertools
-from image_processing import file_extension
 
 j=os.path.join
 dirname=os.path.dirname
@@ -17,7 +16,7 @@ Image.MAX_IMAGE_PIXELS = 194221750
 
 def build_cli_parser():
     parser = argparse.ArgumentParser('Weights Function.')
-    parser.add_argument('dataset_path', help='Path to dataset to crop.')
+    parser.add_argument('dataset_path', help='Path to training folder of dataset.')
     parser.add_argument('-o', '--output', default='weights',
                         help='Path to calculated weights')
     return parser
@@ -35,6 +34,8 @@ def calculate_weights(dataset_path,destination_path ,filetype) :
     classes= list(itertools.chain(*unique_classes))
     n_classes=len(list(np.unique(classes)))
     
+    print("There are", n_classes, "classes found in the masked images.")
+    
     #script to count the number of pixels containing each of the unique values
     n = np.zeros(n_classes, dtype=np.int64)
     imgsize=0
@@ -46,11 +47,27 @@ def calculate_weights(dataset_path,destination_path ,filetype) :
             x=sum(np.sum(img.astype(int) == i, axis=1))
             n[i]+=x[0]
     
+    print("Total number of pixels belonging to each calculated.")
     #Equations taken from Crowdsourcing dataset supplementary material
     ratio= [x / imgsize for x in n]
     weights=[1-x for x in ratio]
     weights[0]=0
-    np.savetxt(j(path,"weights.csv"), weights, delimiter=',') 
+    np.savetxt(j(destination_path,"weights.csv"), weights, delimiter=',') 
+    
+def file_extension(dataset_path) :
+    orig_path=j(dataset_path,"masks")
+    ListFiles = os.walk(orig_path)
+    SplitTypes = []
+    for walk_output in ListFiles:
+        for file_name in walk_output[-1]:
+            SplitTypes.append(file_name.split(".")[-1])
+    x=list(set(SplitTypes))
+    if (len(x)>1) :
+        print('DATA PROCESSING ERROR: Data file not prepared properly. Check the output from prepare_*.py')
+    elif (len(x)==1) :
+        file_ext=x[0]
+        print("All images have a file extension:", file_ext)
+    return file_ext
 
 if __name__ == '__main__':
     parser = build_cli_parser()
