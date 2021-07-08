2df76ca2af06cbc65832f99b6ce4173f3e01f6a2
Hiba Khalidi <hk502@cam.ac.uk>
2021-07-08 11:58:49 +0100

    Tweaked the argparser argumnents and process the class weights in this script

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ Training module.
 import logging
 from shutil import rmtree
 
-import fire
+#import fire
 from utils.metrics import accuracy
 from utils.metrics import dice
 import argparse
@@ -35,6 +35,8 @@ def build_cli_parser():
      help='Rescaling Factor, a number between 0 and 1')
     parser.add_argument('--momentum', default=0.9, type=float,
      help='Momentum term, a number between 0 and 1')
+    parser.add_argument('--swap0', action="store_true",
+     help='Swap labels  0 and 1 (amgad dataset)')
     parser.add_argument('-m', '--multiscale_range', default=None, type=float, nargs='+',
      help='multiscale_range, takes 2 numbers, where the first number passed is less than the second number. Both numbers can be any values between 0 and 1.')
     return parser
@@ -45,8 +47,8 @@ def read_class_weights(weights_file):
         reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
         for row in reader: # each row is a list
             results.append(row)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    list_to_tensor = torch.FloatTensor(results).squeeze(1)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    list_to_tensor = torch.tensor(results, dtype=torch.float, device=device).squeeze(1)
     return list_to_tensor
 
 def fit(dataset_path, model='wesup', **kwargs):
@@ -69,7 +71,13 @@ if __name__ == '__main__':
     parser = build_cli_parser()
     args = parser.parse_args()
     
-    fit(args.dataset_path, model= "wesup", class_weights=args.class_weights, n_classes=args.n_classes,
+    if args.class_weights is not None:
+        weights=read_class_weights(args.class_weights)
+       
+    else:
+        weights=None
+
+    fit(args.dataset_path, model= "wesup", class_weights=weights, n_classes=args.n_classes,
      epochs=args.epochs, batch_size=args.batch, proportion=args.proportion, checkpoint=args.checkpoint,
-     rescale_factor=args.rescale_factor, multiscale_range=args.multiscale_range, momentum=args.momentum)
+     rescale_factor=args.rescale_factor, multiscale_range=args.multiscale_range, momentum=args.momentum)#,swap0=args.swap0)
     #fire.Fire(fit)
