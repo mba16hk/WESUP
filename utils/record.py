e10db03bdba3dea781814ba44539636dee3f0a94
Hiba Khalidi <hk502@cam.ac.uk>
2021-07-23 11:17:42 +0100

    Removal of confusion matrix plotting - moved to infer.py

--- utils/record.py
+++ utils/record.py
@@ -7,7 +7,6 @@ import glob
 import json
 import torch
 from shutil import copyfile, copytree, rmtree
-from sklearn.metrics import confusion_matrix
 from datetime import datetime
 from pathlib import Path
 import numpy as np
@@ -110,53 +109,4 @@ def plot_learning_curves(history_path):
         plt.grid(True)
         plt.savefig(curves_dir / f'{key}.png')
         plt.close()
-
-def plot_confusion_matrix(record_dir, cm, n_classes, normalize=False,  cmap=None):
-
-    """Confusion matrix plotting.
-
-    Arguments:
-        record_dir: Directory where the confusion matrix will be saved
-        cm : The confusion matrix
-        n_classes : The number of classes in the masks
-
-    Citation: http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-
-    """
-    target_names = list(range(n_classes))
-    accuracy = np.trace(cm) / np.sum(cm).astype('float')
-    misclass = 1 - accuracy
-
-    if cmap is None:
-        cmap = plt.get_cmap('Blues')
-
-    plt.figure()
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title('Confusion matrix')
-    #plt.colorbar(cm,fraction=0.046, pad=0.04)
-
-    if target_names is not None:
-        tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=0)
-        plt.yticks(tick_marks, target_names)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-
-    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if normalize:
-            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-        else:
-            plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
-    plt.savefig(record_dir / 'Confusion_Matrix.pdf')
+        
