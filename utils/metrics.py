Fixed Dice calculation

utils/metrics.py
@@ -136,7 +136,7 @@ def dice(S, G, n_classes=2, epsilon=1e-7):
                 S_labels = (S == i)
                 G_labels = (G == i)
                 intersection_lab = np.logical_and(S_labels.cpu(), G_labels.cpu()).sum().item()
-                union = G_labels.sum().item() + S_labels.sum().item() - intersection_lab
+                union = G_labels.sum().item() + S_labels.sum().item() + epsilon
                 alpha = 1/(union**2)
                 numerator += (alpha*intersection_lab)
                 denominator += (alpha*union)
@@ -158,7 +158,7 @@ def dice(S, G, n_classes=2, epsilon=1e-7):
             S_labels = (S == i)
             G_labels = (G == i)
             intersection_lab = np.logical_and(S_labels.cpu(), G_labels.cpu()).sum().item()
-            union = G_labels.sum().item() + S_labels.sum().item() - intersection_lab
+            union = G_labels.sum().item() + S_labels.sum().item() + epsilon
             alpha = 1/(union**2)
             numerator += (alpha*intersection_lab)
             denominator += (alpha*union)